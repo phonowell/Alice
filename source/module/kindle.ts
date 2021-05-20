581de6057cbbe0e3415c121dc6c5b17e05968305
Mimiko <phonowell@gmail.com>
2021-05-20 15:11:51 +0800

    project updated

--- source/module/kindle.ts
+++ source/module/kindle.ts
@@ -17,19 +17,30 @@ import $write_ from 'fire-keeper/write_'
 const path = {
   document: '/Volumes/Kindle/documents',
   kindlegen: '~/OneDrive/程序/kindlegen/kindlegen',
-  storage: '~/OneDrive/书籍/同步/*.txt',
+  storage: '~/OneDrive/书籍/同步',
   temp: './temp/kindle',
 } as const
 
+const html = [
+  '<html lang="zh-cmn-Hans">',
+  '<head>',
+  '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>',
+  '</head>',
+  '<body>',
+  '{{content}}',
+  '</body>',
+  '</html>',
+].join('')
+
 // function
 
-const checkUnicode = async () => {
+const checkUnicode = async (): Promise<boolean> => {
 
   const sub = async (
     source: string,
   ) => !~(await $read_<string>(source)).search(/我/u)
 
-  const listSource = await $source_(path.storage)
+  const listSource = await $source_(`${path.storage}/*.txt`)
   const listResult = await Promise.all(listSource.map(sub))
   const listOutput: string[] = []
   listResult.forEach((result, i) => {
@@ -42,13 +53,11 @@ const checkUnicode = async () => {
   return listOutput.length === 0
 }
 
-const clean = async () => {
-  await $remove_(path.temp)
-}
+const clean = (): Promise<void> => $remove_(path.temp)
 
 const html2mobi = async (
   source: string,
-) => {
+): Promise<void> => {
 
   const { basename } = $getName(source)
   const target = `${path.temp}/${basename}.html`
@@ -63,72 +72,115 @@ const html2mobi = async (
   await $exec_(cmd)
 }
 
+const image2html = async (
+  source: string,
+): Promise<void> => {
+
+  const { basename } = $getName(source)
+  const target = `${path.temp}/${basename}.html`
+
+  const listImage = await $source_(`${source}/*.jpg`)
+  const listResult = (await Promise.all(listImage.map(src => $read_<Buffer>(src))))
+    .map(buffer => buffer.toString('base64'))
+    .map((data, i) => `<p><img alt='${1 + i}.jpg' src='data:image/jpeg;base64,${data}'></p>`)
+
+  const content = html.replace('{{content}}', listResult.join('\n'))
+  await $write_(target, content)
+}
+
 const isExistedOnKindle = async (
   source: string,
-) => {
+): Promise<boolean> => {
 
   const { basename } = $getName(source)
   return $isExisted_(`${path.document}/${basename}.mobi`)
 }
 
-const main = async () => {
+const main = async (): Promise<void> => {
 
   if (!await validateEnvironment()) return
 
-  await renameBook()
+  await renameManga()
+  await renameNovel()
   if (!await checkUnicode()) return
 
-  const sub = async (
+  const subManga = async (
     source: string,
   ) => {
-
     if (await isExistedOnKindle(source)) return
+    await image2html(source)
+    await html2mobi(source)
+    await moveToKindle(source)
+  }
 
+  const subNovel = async (
+    source: string,
+  ) => {
+    if (await isExistedOnKindle(source)) return
     await txt2html(source)
     await html2mobi(source)
     await moveToKindle(source)
   }
 
-  await Promise.all((await $source_(path.storage)).map(sub))
+  await Promise.all((await $source_([`${path.storage}/*`, `!${path.storage}/*.txt`])).map(subManga))
+  await Promise.all((await $source_(`${path.storage}/*.txt`)).map(subNovel))
   await clean()
 }
 
 const moveToKindle = async (
   source: string,
-) => {
+): Promise<void> => {
 
   const { basename } = $getName(source)
   await $copy_(`${path.temp}/${basename}.mobi`, path.document)
 }
 
-const renameBook = async () => {
+const rename = (
+  input: string,
+): string => input
+  .replace(/,/gu, '，')
+  .replace(/:/gu, '：')
+  .replace(/\(/gu, '（')
+  .replace(/\)/gu, '）')
+  .replace(/</gu, '《')
+  .replace(/>/gu, '》')
+  .replace(/\[/gu, '【')
+  .replace(/\]/gu, '】')
+
+const renameManga = async () => {
 
   const sub = async (
     source: string,
   ) => {
-
     const { basename } = $getName(source)
-
-    const _basename = basename
-      .replace(/,/gu, '，')
-      .replace(/:/gu, '：')
-      .replace(/\(/gu, '（')
-      .replace(/\)/gu, '）')
-      .replace(/</gu, '《')
-      .replace(/>/gu, '》')
-      .replace(/\[/gu, '【')
-      .replace(/\]/gu, '】')
-
+    const _basename = rename(basename)
     if (_basename === basename) return
+
+    const _source = source.replace(basename, _basename)
+    await $exec_(`mv "${source}" "${_source}"`)
+  }
+
+  await Promise.all((await $source_([`${path.storage}/*`, `!${path.storage}/*.txt`])).map(sub))
+}
+
+const renameNovel = async () => {
+
+  const sub = async (
+    source: string,
+  ) => {
+    const { basename } = $getName(source)
+    const _basename = rename(basename)
+    if (_basename === basename) return
+
     await $rename_(source, { basename: _basename })
   }
 
-  await Promise.all((await $source_(path.storage)).map(sub))
+  await Promise.all((await $source_(`${path.storage}/*.txt`)).map(sub))
 }
 
 const txt2html = async (
   source: string,
-) => {
+): Promise<void> => {
 
   const { basename } = $getName(source)
   const target = `${path.temp}/${basename}.html`
@@ -136,29 +188,19 @@ const txt2html = async (
   const listContent = (
     await $read_<string>(source)
   ).split('\n')
-  const listResult: string[] = []
 
+  const listResult: string[] = []
   for (let line of listContent) {
     line = line.trim()
     if (!line) continue
     listResult.push(`<p>${line}</p>`)
   }
 
-  const content = [
-    '<html lang="zh-cmn-Hans">',
-    '<head>',
-    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>',
-    '</head>',
-    '<body>',
-    listResult.join('\n'),
-    '</body>',
-    '</html>',
-  ]
-
-  await $write_(target, content.join(''))
+  const content = html.replace('{{content}}', listResult.join('\n'))
+  await $write_(target, content)
 }
 
-const validateEnvironment = async () => {
+const validateEnvironment = async (): Promise<boolean> => {
 
   if (!$os('macos')) {
     $info(`invalid os '${$os()}'`)
--- task/z.ts
+++ task/z.ts
@@ -1,9 +1,12 @@
-import $sleep_ from 'fire-keeper/sleep_'
+import $ from 'fire-keeper'
 
 // function
 
 const main = async () => {
-  await $sleep_(0)
+  $.info(await $.source_([
+    '~/OneDrive/书籍/同步/*',
+    '!~/OneDrive/书籍/同步/*.txt',
+  ]))
 }
 
 // export
